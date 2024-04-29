b830973e72fa4942872f25a2b96ab60016455750
vamik <vamiksiddiqui666@gmail.com>
2024-04-29 19:50:42 +0530

    input components

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Button({
+    children,
+    type = 'button',
+    bgColor = 'bg-blue-600',
+    textColor = 'text-white',
+    className = '',
+    ...props
+}) {
+    return (
+        <button
+            className={`${bgColor} ${textColor} ${className}`}
+            type={`${type}`}
+            {...props}
+        >{children}</button>
+    )
+}
