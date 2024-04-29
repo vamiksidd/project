e30eb3d808b0bb1123810c9d0154377257d8afa9
vamik <vamiksiddiqui666@gmail.com>
2024-04-29 16:24:01 +0530

    store configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
-
-
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux' //for using redux with react
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components'
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      if (userData) {
+        dispatch(login({ userData }))
+      } else {
+        dispatch(logout())
+      }
+    }).finally(() => setLoading(false))
+  }, [])
   return (
     <>
-
+      {!loading ? (
+        <div className='min-h-screen  flex flex-wrap content-between  bg-gray-400'>
+          <div className='block'>
+            <Header />
+            <main>TODO</main>
+            <Footer />
+          </div>
+        </div>
+      ) : null}
     </>
   )
 }
